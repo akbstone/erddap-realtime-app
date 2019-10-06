5eaadbde517fd5f179f769b3a0b8cdf05b440311
unknown <brian@axiomdatascience.com - laptop>
2019-10-06 09:07:02 -0800

    use separate sensor component

--- src/components/station_display/station.js
+++ src/components/station_display/station.js
@@ -1,67 +1,32 @@
 import React from 'react'
-import {select} from 'd3-selection';
-import {erddapTimeseriesChart} from 'erddap-timeseries-chart';
+import Sensor from './sensor';
 
 class Station extends React.Component {
   
-  componentDidMount(){
-
-      let width = 500,
-          height = 200,
-          data = [
-              {
-                  time:new Date(2019,10,1),
-                  value:10,
-                  qc:1
-              },
-              {
-                  time:new Date(2019,10,2),
-                  value:11,
-                  qc:3
-              },
-              {
-                  time:new Date(2019,10,3),
-                  value:4,
-                  qc:3
-              },
-              {
-                  time:new Date(2019,10,4),
-                  value:5,
-                  qc:4
-              },
-              {
-                  time:new Date(2019,10,5),
-                  value:2,
-                  qc:1
-              }
-
-          ],
-          svg = select(this.refs.chart)
-                .append('svg')
-                    .attr('width',width)
-                    .attr('height',height),
-
-          chart = erddapTimeseriesChart()
-                  .data(data)
-                  .width(width)
-                  .height(height)
-                  .x(d=>d.time)
-                  .y(d=>d.value)
-                  .chartType('line'),
-
-          chartSvg = select(this.refs.chart)
-                .append('svg')
-                .attr('width',width)
-                .attr('height',height)
-                .call(chart);
-  }
-
   render() {
+    let sensors = [
+        {
+            label:'Air temperature',
+            index:1,
+            id:'air_temperature'
+        },
+        {
+            label:'Water temperature',
+            index:2,
+            id:'water_temperature'
+        },
+        {
+            label:'Salinity',
+            index:3,
+            id:'salinity'
+        }
+
+    ]
     return <div>
         <h1>Station name ({this.props.match.params.id})</h1>
-        <div className="chartWrap" ref="chartWrap">
-          <svg width="760" height="200" className="chart" ref="chart"></svg>
-        </div>
+        {sensors.map(sensor => (
+            <Sensor key={sensor.index} label={sensor.label} height={100} />
+        ))}
       </div>
   }
 }
