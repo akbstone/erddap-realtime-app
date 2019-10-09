00ae2fd2c48b057082f14b143846325ed06888ac
unknown <brian@axiomdatascience.com - laptop>
2019-10-09 10:44:45 -0400

    use erddap parser to load live metadata/data

--- src/components/station_display/station.js
+++ src/components/station_display/station.js
@@ -9,34 +9,43 @@ class Station extends React.Component {
         server:'https://erddap.sensors.axds.co/erddap',
         dataset_id:this.props.match.params.id
       })
+      if(Array.isArray(stationMetadata)){
+          stationMetadata = {
+              parameters:stationMetadata.slice().map(p=>{
+                  return Object.assign(
+                    p,
+                    {
+                      dataset_id:this.props.match.params.id,
+                      variable_name:p['Variable Name']
+                    }
+                  )
+              }),
+              label:this.props.match.params.id
+          }
+      }
       this.setState({station:stationMetadata})
 
   }
   render() {
-    let sensors = [
-        {
-            label:'Air temperature',
-            index:1,
-            id:'air_temperature'
-        },
-        {
-            label:'Water temperature',
-            index:2,
-            id:'water_temperature'
-        },
-        {
-            label:'Salinity',
-            index:3,
-            id:'salinity'
-        }
-
-    ]
-    return <div>
+    let output = 'Loading..',
+        state = this.state;
+    if(state && state.station){
+        let parameters = this.state.station.parameters.slice().map((d,i)=>{
+            return Object.assign(
+                d,
+                {
+                    index:i
+                }
+            )
+        })
+        output = <div className="station-wrap">
         <h1>Station name ({this.props.match.params.id})</h1>
-        {sensors.map(sensor => (
-            <Sensor key={sensor.index} label={sensor.label} height={100} />
+        {parameters.map(parameter => (
+            <Sensor key={parameter.index} parameter={parameter} height={100} />
         ))}
       </div>
+    }
+    return output;
   }
 }
 export default Station
