aa79703d350aeabf0eeb7a1ca7847ada6dfafb02
Brian Stone <brian@axiomdatascience.com>
2020-05-06 22:11:07 -0800

    highlight latest time period by default

--- src/components/station_display/sensor.js
+++ src/components/station_display/sensor.js
@@ -19,37 +19,39 @@ class Sensor extends React.Component {
   async componentDidMount(){
     
     try {
+
+        select(this.refs.chartWrap).select('*').empty();
+
         let now = new Date(),
             day = 24*60*60*1000,
             start = new Date(+now - 10*day),
             end = now,
-            variable_names = {time:'time'}
-
-        variable_names[this.props.parameter.variable_name] = 'value';
-
-        let data = await erddapParser.getTabledapData({
-            server: process.env.REACT_APP_ERDDAP_URL || 'https://erddap.sensors.axds.co/erddap',
-            dataset_id:this.props.parameter.dataset_id,
-            variables:[
-                'time',
-                this.props.parameter.variable_name
-            ],
-            constraints:{
-                'time>=':start,
-                'time<=':end
-            },
-            variable_names:variable_names
-        })
-  
-
-        select(this.refs.chartWrap).select('*').empty();
-        let width = this.props.width || this.refs.chartWrap.offsetWidth,
+            variable_names = {time:'time'},
+            width = this.props.width || this.refs.chartWrap.offsetWidth,
             height = this.props.height || 200,
             svg = select(this.refs.chartWrap)
                     .append('svg')
                         .attr('width',width)
-                        .attr('height',height),
+                        .attr('height',height);
 
+        variable_names[this.props.parameter.variable_name] = 'value';
+
+        let data = await erddapParser.getTabledapData({
+                server: process.env.REACT_APP_ERDDAP_URL || 'https://erddap.sensors.axds.co/erddap',
+                dataset_id:this.props.parameter.dataset_id,
+                variables:[
+                    'time',
+                    this.props.parameter.variable_name
+                ],
+                constraints:{
+                    'time>=':start,
+                    'time<=':end
+                },
+                variable_names:variable_names
+            }),
+            highlightLast = ()=>{
+                chart.highlightAtX(width)
+            },
             chart = erddapTimeseriesChart()
                     .data(data)
                     .width(width)
@@ -58,9 +60,11 @@ class Sensor extends React.Component {
                     .y(d=>d.value)
                     .chartType('line')
                     .on('mousemove',this.onMouseMove)
-                    .on('mouseout',this.onMouseOut)
+                    .on('mouseout',highlightLast);
             
             svg.call(chart);
+            
+            highlightLast();
         }catch(e){
             throw(e)
         }
@@ -78,8 +82,8 @@ class Sensor extends React.Component {
         
     return <div style={{clear:"both"}}>
         <h4>{this.props.parameter.long_name} ({this.props.parameter.units})</h4>
-        <div style={{float:"right",width:"100px"}}><SensorHighlight highlight={this.state.highlight} parameter={this.props.parameter} /></div>
-        <div className="chartWrap" ref="chartWrap" style={{marginRight:"100px"}}>
+        <div style={{float:"right",width:"120px"}}><SensorHighlight highlight={this.state.highlight} parameter={this.props.parameter} /></div>
+        <div className="chartWrap" ref="chartWrap" style={{marginRight:"120px"}}>
         </div>
       </div>
   }
--- src/components/station_display/sensorHighlight.js
+++ src/components/station_display/sensorHighlight.js
@@ -6,7 +6,7 @@ function SensorHighlight({ highlight }){
     if (highlight){
         return (
             <React.Fragment>
-                <h3>
+                <h3 style={{fontSize:"28px"}}>
                     <b>{highlight.value.toFixed(2)}</b>
                 </h3>
                 <span style={{fontSize:"10px"}}>
